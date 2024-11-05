a6456dfbe4dd89a3e555ce407f28a11576997d6b
David Grammatico <david.lugdunum@gmail.com>
2024-11-05 13:42:29 +0100

    add observer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,55 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Observable, Subscription } from 'rxjs';
+
+export type CallBackAvecString = (message: string) => void;
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [AsyncPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'rxjs-angular18';
+export class AppComponent implements OnInit, OnDestroy {
+  observable$!: Observable<string>;
+  private subscription = new Subscription();
+  ngOnInit() {
+    const callBack: CallBackAvecString = (message: string) =>
+      console.info(message.toUpperCase());
+    console.info('-------------------------');
+    // setTimeout(() => {
+    //   console.info('0. Async setTimeout');
+    // }, 1500);
+
+    //EAGER
+    const promise = new Promise<string>((resolve, reject) => {
+      console.info('1. Async Promise');
+
+      resolve(' P => Promise resolved');
+    }).then(callBack);
+
+    // LAZY
+    this.observable$ = new Observable<string>((observer) => {
+      console.info('2. Async Observable');
+      observer.next('O1 => Observable next');
+      setTimeout(() => {
+        observer.next('O2 => Observable next');
+        observer.complete();
+      }, 1500);
+    });
+    let sub = this.observable$.pipe(takeUntilDestroyed()).subscribe({
+      next: callBack,
+      complete: () => console.info('O => Observable complete'),
+    });
+
+    this.subscription.add(sub);
+
+    console.info('*************************');
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
